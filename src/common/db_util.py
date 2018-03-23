993b411061642476c621fe8e4af5d2135af27bd0
matsumatsu20 <matsumatsu20@gmail.com>
2018-03-24 00:21:15 +0900

    Add utility methods to db util and Create custom error

--- src/common/db_util.py
+++ src/common/db_util.py
@@ -1,6 +1,7 @@
 import os
 from boto3.dynamodb.conditions import Key
-
+from record_not_found_error import RecordNotFoundError
+from not_authorized_error import NotAuthorizedError
 
 class DBUtil:
 
@@ -8,7 +9,34 @@ class DBUtil:
     def exists_public_article(dynamodb, article_id):
         query_params = {
             'IndexName': 'article_id-status_key-index',
-            'KeyConditionExpression': Key('status').eq('public') & Key('article_id').eq(article_id)
+            'KeyConditionExpression': Key('status').eq('public') & Key('article_id').eq(article_id),
+            'Select': 'COUNT'
         }
         article_info_table = dynamodb.Table(os.environ['ARTICLE_INFO_TABLE_NAME'])
         return True if article_info_table.query(**query_params)['Count'] == 1 else False
+
+    @staticmethod
+    def exists_article(dynamodb, article_id, user_id=None, status=None):
+        article_info_table = dynamodb.Table(os.environ['ARTICLE_INFO_TABLE_NAME'])
+        article_info = article_info_table.get_item(Key={'article_id': article_id}).get('Item')
+
+        if article_info is None:
+            return False
+        if user_id is not None and article_info['user_id'] != user_id:
+            return False
+        if status is not None and article_info['status'] != status:
+            return False
+        return True
+
+    @staticmethod
+    def validate_article_existence(dynamodb, article_id, user_id=None, status=None):
+        article_info_table = dynamodb.Table(os.environ['ARTICLE_INFO_TABLE_NAME'])
+        article_info = article_info_table.get_item(Key={'article_id': article_id}).get('Item')
+
+        if article_info is None:
+            raise RecordNotFoundError('Record Not Found')
+        if user_id is not None and article_info['user_id'] != user_id:
+            raise NotAuthorizedError('Forbidden')
+        if status is not None and article_info['status'] != status:
+            raise RecordNotFoundError('Record Not Found')
+        return True
