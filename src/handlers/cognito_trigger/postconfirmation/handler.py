c6c959399f5bd53abf51c07364f2b2e093d7547a
yamasita <yamasita@yaasita.net>
2018-03-26 14:13:09 +0900

    write to the Users table only ID

--- src/handlers/cognito_trigger/postconfirmation/handler.py
+++ src/handlers/cognito_trigger/postconfirmation/handler.py
@@ -8,10 +8,7 @@ dynamodb = boto3.resource('dynamodb')
 def lambda_handler(event, context):
     users = dynamodb.Table(os.environ['USERS_TABLE_NAME'])
     user = {
-            'user_id': event["userName"],
-            'sub': event["request"]["userAttributes"]["sub"],
-            'email': event["request"]["userAttributes"]["email"],
-            'phone_number': event["request"]["userAttributes"]["phone_number"]
+            'user_id': event["userName"]
             }
     try:
         users.put_item(Item=user, ConditionExpression='attribute_not_exists(user_id)')
