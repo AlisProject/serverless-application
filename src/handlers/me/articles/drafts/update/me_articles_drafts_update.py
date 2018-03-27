f8f520015cbfefd381af2e80d715f3efedcb637e
matsumatsu20 <matsumatsu20@gmail.com>
2018-03-27 21:44:40 +0900

    Refactor using self.params

--- src/handlers/me/articles/drafts/update/me_articles_drafts_update.py
+++ src/handlers/me/articles/drafts/update/me_articles_drafts_update.py
@@ -31,51 +31,48 @@ class MeArticlesDraftsUpdate(LambdaBase):
         if not self.event.get('body') or not json.loads(self.event.get('body')):
             raise ValidationError('Request parameter is required')
 
-        validate(self.event.get('pathParameters'), self.get_schema())
-        validate(json.loads(self.event.get('body')), self.get_schema(), format_checker=FormatChecker())
+        validate(self.params, self.get_schema(), format_checker=FormatChecker())
 
     def exec_main_proc(self):
         DBUtil.validate_article_existence(
             self.dynamodb,
-            self.event['pathParameters']['article_id'],
+            self.params['article_id'],
             user_id=self.event['requestContext']['authorizer']['claims']['cognito:username'],
             status='draft'
         )
 
-        params = json.loads(self.event.get('body'))
-
-        self.__update_article_content(params)
-        self.__update_article_info(params)
+        self.__update_article_content()
+        self.__update_article_info()
 
         return {
             'statusCode': 200
         }
 
-    def __update_article_info(self, params):
+    def __update_article_info(self):
         article_info_table = self.dynamodb.Table(os.environ['ARTICLE_INFO_TABLE_NAME'])
 
         article_info_table.update_item(
             Key={
-                'article_id': self.event['pathParameters']['article_id'],
+                'article_id': self.params['article_id'],
             },
             UpdateExpression="set title = :title, overview=:overview, eye_catch_url=:eye_catch_url",
             ExpressionAttributeValues={
-                ':title': TextSanitizer.sanitize_text(params.get('title')),
-                ':overview': TextSanitizer.sanitize_text(params.get('overview')),
-                ':eye_catch_url': params.get('eye_catch_url')
+                ':title': TextSanitizer.sanitize_text(self.params.get('title')),
+                ':overview': TextSanitizer.sanitize_text(self.params.get('overview')),
+                ':eye_catch_url': self.params.get('eye_catch_url')
             }
         )
 
-    def __update_article_content(self, params):
+    def __update_article_content(self):
         article_content_table = self.dynamodb.Table(os.environ['ARTICLE_CONTENT_TABLE_NAME'])
 
         article_content_table.update_item(
             Key={
-                'article_id': self.event['pathParameters']['article_id'],
+                'article_id': self.params['article_id'],
             },
             UpdateExpression="set title = :title, body=:body",
             ExpressionAttributeValues={
-                ':title': TextSanitizer.sanitize_text(params.get('title')),
-                ':body': TextSanitizer.sanitize_article_body(params.get('body'))
+                ':title': TextSanitizer.sanitize_text(self.params.get('title')),
+                ':body': TextSanitizer.sanitize_article_body(self.params.get('body'))
             }
         )
