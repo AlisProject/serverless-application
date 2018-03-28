c82d6392b10393869f2ec30edcc51acc042ba8cc
keillera <keikaillera@gmail.com>
2018-03-28 22:50:29 +0900

    Add API that show user info.

---
+++ src/handlers/users/info/show/users_info_show.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import settings
+from lambda_base import LambdaBase
+from jsonschema import validate
+from decimal_encoder import DecimalEncoder
+
+
+class UsersInfoShow(LambdaBase):
+    def get_schema(self):
+        return {
+            'type': 'object',
+            'properties': {
+                'user_id': settings.parameters['user_id'],
+            },
+            'required': ['user_id']
+        }
+
+    def validate_params(self):
+        # single
+        validate(self.params, self.get_schema())
+
+    def exec_main_proc(self):
+        users_table = self.dynamodb.Table(os.environ['USERS_TABLE_NAME'])
+        response = users_table.get_item(Key={'user_id': self.params['user_id']})
+
+        if response.get('Item') is None:
+            return {
+                'statusCode': 404,
+                'body': json.dumps({'message': 'Record Not Found'})
+            }
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response['Item'], cls=DecimalEncoder)
+        }
