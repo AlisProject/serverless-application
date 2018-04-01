b4248bdc7658710105ccb718fbcec1cc70635df4
keillera <keikaillera@gmail.com>
2018-04-01 20:50:30 +0900

    Fix a typo

--- src/handlers/me/articles/images/create/handler.py
+++ src/handlers/me/articles/images/create/handler.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import boto3
-from me_articles_images_post import MeArticlesImagesPost
+from me_articles_images_create import MeArticlesImagesCreate
 
 dynamodb = boto3.resource('dynamodb')
 s3 = boto3.resource('s3')
 
 
 def lambda_handler(event, context):
-    articles_images_post = MeArticlesImagesPost(event=event, context=context, dynamodb=dynamodb, s3=s3)
-    return articles_images_post.main()
+    me_articles_images_create = MeArticlesImagesCreate(event=event, context=context, dynamodb=dynamodb, s3=s3)
+    return me_articles_images_create.main()
