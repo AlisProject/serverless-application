4485955545a9a1d547fac5848071ca4abf2e5fc5
matsumatsu20 <matsumatsu20@gmail.com>
2018-04-03 23:18:36 +0900

    Fix a class name and variables

--- src/handlers/me/articles/public/edit/handler.py
+++ src/handlers/me/articles/public/edit/handler.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import boto3
-from me_articles_drafts_show import MeArticlesDraftsShow
+from me_articles_public_edit import MeArticlesPublicEdit
 
 dynamodb = boto3.resource('dynamodb')
 
 
 def lambda_handler(event, context):
-    me_articles_drafts_show = MeArticlesDraftsShow(event, context, dynamodb)
-    return me_articles_drafts_show.main()
+    me_articles_public_edit = MeArticlesPublicEdit(event, context, dynamodb)
+    return me_articles_public_edit.main()
