1dd899e3bd57349c2f58e14c2d9a1d067b045e17
matsumatsu20 <matsumatsu20@gmail.com>
2018-04-15 21:35:49 +0900

    Separate CFn files

--- make_template.py
+++ make_template.py
@@ -4,7 +4,7 @@ import os
 from jinja2 import Template, Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('template.yaml.tpl')
+target_template_names = ['template.yaml.tpl', 'cognito-template.yaml.tpl', 'database-template.yaml.tpl']
 
 data = {
     'COGNITO_EMAIL_VERIFY_URL': os.environ['COGNITO_EMAIL_VERIFY_URL'],
@@ -16,8 +16,23 @@ data = {
     'MIN_DYNAMO_WRITE_CAPACITTY': os.environ['MIN_DYNAMO_WRITE_CAPACITTY'],
     'MAX_DYNAMO_WRITE_CAPACITTY': os.environ['MAX_DYNAMO_WRITE_CAPACITTY'],
     'MIN_DYNAMO_READ_CAPACITTY': os.environ['MIN_DYNAMO_READ_CAPACITTY'],
-    'MAX_DYNAMO_READ_CAPACITTY': os.environ['MAX_DYNAMO_READ_CAPACITTY']
+    'ARTICLE_INFO_TABLE_NAME': os.environ['ARTICLE_INFO_TABLE_NAME'],
+    'ARTICLE_CONTENT_TABLE_NAME': os.environ['ARTICLE_CONTENT_TABLE_NAME'],
+    'ARTICLE_HISTORY_TABLE_NAME': os.environ['ARTICLE_HISTORY_TABLE_NAME'],
+    'ARTICLE_CONTENT_EDIT_TABLE_NAME': os.environ['ARTICLE_CONTENT_EDIT_TABLE_NAME'],
+    'ARTICLE_EVALUATED_MANAGE_TABLE_NAME': os.environ['ARTICLE_EVALUATED_MANAGE_TABLE_NAME'],
+    'ARTICLE_ALIS_TOKEN_TABLE_NAME': os.environ['ARTICLE_ALIS_TOKEN_TABLE_NAME'],
+    'ARTICLE_LIKED_USER_TABLE_NAME': os.environ['ARTICLE_LIKED_USER_TABLE_NAME'],
+    'ARTICLE_FRAUD_USER_TABLE_NAME': os.environ['ARTICLE_FRAUD_USER_TABLE_NAME'],
+    'ARTICLE_PV_USER_TABLE_NAME': os.environ['ARTICLE_PV_USER_TABLE_NAME'],
+    'ARTICLE_SCORE_TABLE_NAME': os.environ['ARTICLE_SCORE_TABLE_NAME'],
+    'USERS_TABLE_NAME': os.environ['USERS_TABLE_NAME'],
+    'BETA_USERS_TABLE_NAME': os.environ['BETA_USERS_TABLE_NAME'],
+    'COGNITO_USER_POOL_ARN': os.environ['COGNITO_USER_POOL_ARN']
 }
 
-with open("template.yaml", "w") as f:
-    f.write(template.render(data))
+for target in target_template_names:
+    template = env.get_template(target)
+
+    with open(target[:target.rfind('.')], "w") as f:
+        f.write(template.render(data))
