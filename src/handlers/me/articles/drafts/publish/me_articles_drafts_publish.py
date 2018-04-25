a9056240eee9fd6fe44342af6a9d0b9132c235ca
matsumatsu20 <matsumatsu20@gmail.com>
2018-04-25 23:47:01 +0900

    Update sort_key and add published_at when article publish

--- src/handlers/me/articles/drafts/publish/me_articles_drafts_publish.py
+++ src/handlers/me/articles/drafts/publish/me_articles_drafts_publish.py
@@ -5,9 +5,11 @@ import logging
 import traceback
 import settings
 import time
+from boto3.dynamodb.conditions import Key
 from lambda_base import LambdaBase
 from jsonschema import validate, ValidationError, FormatChecker
 from db_util import DBUtil
+from time_util import TimeUtil
 
 
 class MeArticlesDraftsPublish(LambdaBase):
@@ -32,7 +34,7 @@ class MeArticlesDraftsPublish(LambdaBase):
 
     def exec_main_proc(self):
         self.__delete_article_content_edit()
-        self.__create_article_history()
+        self.__create_article_history_and_update_sort_key()
 
         article_info_table = self.dynamodb.Table(os.environ['ARTICLE_INFO_TABLE_NAME'])
 
@@ -56,11 +58,28 @@ class MeArticlesDraftsPublish(LambdaBase):
         if article_content_edit:
             article_content_edit_table.delete_item(Key={'article_id': self.params['article_id']})
 
-    def __create_article_history(self):
+    def __create_article_history_and_update_sort_key(self):
+        # update sort_key
+        article_history_table = self.dynamodb.Table(os.environ['ARTICLE_HISTORY_TABLE_NAME'])
+        article_histories = article_history_table.query(
+            KeyConditionExpression=Key('article_id').eq(self.params['article_id'])
+        )['Items']
+
+        if len(article_histories) == 0:
+            sort_key = TimeUtil.generate_sort_key()
+            article_info_table = self.dynamodb.Table(os.environ['ARTICLE_INFO_TABLE_NAME'])
+            article_info_table.update_item(
+                Key={
+                    'article_id': self.params['article_id'],
+                },
+                UpdateExpression='set sort_key = :sort_key, published_at = :published_at',
+                ExpressionAttributeValues={':sort_key': sort_key, ':published_at': int(time.time())}
+            )
+
+        # create article_history
         article_content_table = self.dynamodb.Table(os.environ['ARTICLE_CONTENT_TABLE_NAME'])
         article_content = article_content_table.get_item(Key={'article_id': self.params['article_id']}).get('Item')
 
-        article_history_table = self.dynamodb.Table(os.environ['ARTICLE_HISTORY_TABLE_NAME'])
         article_history_table.put_item(
             Item={
                 'article_id': article_content['article_id'],
