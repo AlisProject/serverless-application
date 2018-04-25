3c68093737ae668e75c1ddcb20ee0fae857ddaec
sot528 <sot528@gmail.com>
2018-04-25 22:19:07 +0900

    fix script

--- src/handlers/me/wallet/balance/me_wallet_balance.py
+++ src/handlers/me/wallet/balance/me_wallet_balance.py
@@ -20,12 +20,17 @@ class MeWalletBalance(LambdaBase):
 
     @staticmethod
     def __get_balance(address):
-        payload = {dict(private_eth_address=address)}
+        headers = {"content-type": "application/json"}
+        payload = json.dumps({"private_eth_address": address[2:]})
         auth = AWSRequestsAuth(aws_access_key=os.environ['PRIVATE_CHAIN_AWS_ACCESS_KEY'],
                                aws_secret_access_key=os.environ['PRIVATE_CHAIN_AWS_SECRET_ACCESS_KEY'],
                                aws_host=os.environ['PRIVATE_CHAIN_EXECUTE_API_HOST'],
                                aws_region='ap-northeast-1',
                                aws_service='execute-api')
         response = requests.post('https://' + os.environ['PRIVATE_CHAIN_EXECUTE_API_HOST'] +
-                                 '/production/wallet/balance', auth=auth, payload=payload)
-        return json.loads(response.text)
+                                 '/production/wallet/balance', auth=auth, headers=headers, data=payload)
+
+        return {
+            'statusCode': 200,
+            'body': '{result: "' + json.loads(response.text)["result"] + '"}',
+        }
