8938fd37c0aa29cbac071afdf3c9772a49f44201
matsumatsu20 <matsumatsu20@gmail.com>
2018-08-04 16:43:01 +0900

    Delete dynamoDB

--- src/handlers/articles/popular/handler.py
+++ src/handlers/articles/popular/handler.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
 
-import boto3
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
 from articles_popular import ArticlesPopular
 
-dynamodb = boto3.resource('dynamodb')
 awsauth = AWS4Auth(
     os.environ['AWS_ACCESS_KEY_ID'],
     os.environ['AWS_SECRET_ACCESS_KEY'],
@@ -25,5 +23,5 @@ elasticsearch = Elasticsearch(
 
 
 def lambda_handler(event, context):
-    articles_popular = ArticlesPopular(event, context, dynamodb=dynamodb, elasticsearch=elasticsearch)
+    articles_popular = ArticlesPopular(event, context, elasticsearch=elasticsearch)
     return articles_popular.main()
