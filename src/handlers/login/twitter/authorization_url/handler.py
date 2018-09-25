c8c3d59c3f999e23346ce05f1b20e6928b2a2dc3
horike37 <horike37@gmail.com>
2018-09-25 21:34:02 +0900

    add twitter oauth api

---
+++ src/handlers/login/twitter/authorization_url/handler.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import boto3
+import os
+import json
+from decimal_encoder import DecimalEncoder
+
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'hello': 'world'}, cls=DecimalEncoder)
+    }
