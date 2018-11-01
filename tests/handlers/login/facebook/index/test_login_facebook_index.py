7dc536d9fd342c0937d67c6846cbba8097456374
horike37 <horike37@gmail.com>
2018-11-01 22:26:15 +0900

    add test for facebook lambda itself

--- tests/handlers/login/facebook/index/test_login_facebook_index.py
+++ tests/handlers/login/facebook/index/test_login_facebook_index.py
@@ -3,19 +3,19 @@ import json
 from botocore.exceptions import ClientError
 from unittest import TestCase
 from unittest.mock import patch
-from login_yahoo_index import LoginYahooIndex
-from exceptions import YahooOauthError
+from login_facebook_index import LoginFacebookIndex
+from exceptions import FacebookOauthError
 from tests_util import TestsUtil
 
 dynamodb = TestsUtil.get_dynamodb_client()
 
 
-class TestLoginYahooIndex(TestCase):
+class TestLoginFacebookIndex(TestCase):
 
     def setUp(self):
-        os.environ['YAHOO_CLIENT_ID'] = 'fake_client_id'
-        os.environ['YAHOO_SECRET'] = 'fake_secret'
-        os.environ['YAHOO_OAUTH_CALLBACK_URL'] = 'http://callback'
+        os.environ['FACEBOOK_APP_ID'] = 'fake_client_id'
+        os.environ['FACEBOOK_APP_SECRET'] = 'fake_secret'
+        os.environ['FACEBOOK_OAUTH_CALLBACK_URL'] = 'http://callback'
         os.environ['EXTERNAL_PROVIDER_LOGIN_COMMON_TEMP_PASSWORD'] = 'xxxxxxxx'
         os.environ['EXTERNAL_PROVIDER_LOGIN_MARK'] = 'xxxxx'
         os.environ['COGNITO_USER_POOL_ID'] = 'user_pool_id'
@@ -24,14 +24,14 @@ class TestLoginYahooIndex(TestCase):
         TestsUtil.delete_all_tables(dynamodb)
         external_provider_users_items = [
             {
-                'external_provider_user_id': 'yahoo-1234',
+                'external_provider_user_id': 'Facebook-1234',
                 'user_display_name': 'test_display_name02',
                 'email': 'test02@example.com',
                 'password': 'test_pass',
                 'iv': 'iv'
             },
             {
-                'external_provider_user_id': 'yahoo-12345',
+                'external_provider_user_id': 'Facebook-12345',
                 'user_display_name': 'test_display_name02',
                 'email': 'test02@example.com',
                 'password': 'test_pass',
@@ -42,7 +42,7 @@ class TestLoginYahooIndex(TestCase):
         TestsUtil.create_table(dynamodb, os.environ['EXTERNAL_PROVIDER_USERS_TABLE_NAME'], external_provider_users_items)
 
     def assert_bad_request(self, params):
-        response = LoginYahooIndex(params, {}).main()
+        response = LoginFacebookIndex(params, {}).main()
         self.assertEqual(response['statusCode'], 400)
 
     def test_validation_with_no_body(self):
@@ -71,18 +71,17 @@ class TestLoginYahooIndex(TestCase):
         self.assert_bad_request(params)
 
     def test_main_ok_with_creating_new_user(self):
-        with patch('login_yahoo_index.YahooUtil') as yahoo_mock, \
-         patch('login_yahoo_index.UserUtil') as user_mock:
-            yahoo_mock.return_value.get_user_info.return_value = {
-                'user_id': 'Yahoo-1234',
-                'email': 'Yahoo-1234@example.com',
+        with patch('login_facebook_index.FacebookUtil') as facebook_mock, \
+         patch('login_facebook_index.UserUtil') as user_mock:
+            facebook_mock.return_value.get_user_info.return_value = {
+                'user_id': 'Facebook-1234',
+                'email': 'Facebook-1234@example.com',
             }
-            yahoo_mock.return_value.get_access_token.return_value = {
+            facebook_mock.return_value.get_access_token.return_value = {
                 'access_token': 'access_token',
                 'id_token': 'id_token'
             }
-            yahoo_mock.return_value.verify_state_nonce.return_value = True
-            yahoo_mock.return_value.verify_access_token.return_value = True
+            facebook_mock.return_value.verify_state_nonce.return_value = True
             user_mock.exists_user.return_value = False
             user_mock.create_external_provider_user.return_value = {
                 'AuthenticationResult': {
@@ -99,7 +98,7 @@ class TestLoginYahooIndex(TestCase):
                 }
             }
             params['body'] = json.dumps(params['body'])
-            response = LoginYahooIndex(params, {}).main()
+            response = LoginFacebookIndex(params, {}).main()
             self.assertEqual(response['statusCode'], 200)
             self.assertEqual(
                 json.loads(response['body']),
@@ -107,26 +106,26 @@ class TestLoginYahooIndex(TestCase):
                     'access_token': 'aaaaa',
                     'id_token': 'bbbbb',
                     'refresh_token': 'ccccc',
-                    'last_auth_user': 'Yahoo-1234',
+                    'last_auth_user': 'Facebook-1234',
                     'has_user_id': False,
                     'status': 'sign_up'
                 }
             )
 
     def test_main_ok_with_existing_user_and_user_id(self):
-        with patch('login_yahoo_index.YahooUtil') as yahoo_mock, \
-         patch('login_yahoo_index.UserUtil') as user_mock:
-            yahoo_mock.return_value.get_user_info.return_value = {
-                'user_id': 'Yahoo-12345',
-                'email': 'Yahoo-1234@example.com',
+        with patch('login_facebook_index.FacebookUtil') as facebook_mock, \
+         patch('login_facebook_index.UserUtil') as user_mock:
+            facebook_mock.return_value.get_user_info.return_value = {
+                'user_id': 'Facebook-12345',
+                'email': 'Facebook-1234@example.com',
                 'display_name': 'my_name'
             }
-            yahoo_mock.return_value.get_access_token.return_value = {
+            facebook_mock.return_value.get_access_token.return_value = {
                 'access_token': 'access_token',
                 'id_token': 'id_token'
             }
-            yahoo_mock.return_value.verify_state_nonce.return_value = True
-            yahoo_mock.return_value.verify_access_token.return_value = True
+            facebook_mock.return_value.verify_state_nonce.return_value = True
+
             user_mock.get_external_provider_password.return_value = 'password'
             user_mock.exists_user.return_value = True
             user_mock.has_user_id.return_value = True
@@ -145,7 +144,7 @@ class TestLoginYahooIndex(TestCase):
                 }
             }
             params['body'] = json.dumps(params['body'])
-            response = LoginYahooIndex(params, {}, dynamodb=dynamodb).main()
+            response = LoginFacebookIndex(params, {}, dynamodb=dynamodb).main()
             self.assertEqual(response['statusCode'], 200)
             self.assertEqual(
                 json.loads(response['body']),
@@ -168,19 +167,19 @@ class TestLoginYahooIndex(TestCase):
             )
 
     def test_main_ok_with_existing_user_and_no_user_id(self):
-        with patch('login_yahoo_index.YahooUtil') as yahoo_mock, \
-         patch('login_yahoo_index.UserUtil') as user_mock:
-            yahoo_mock.return_value.get_user_info.return_value = {
-                'user_id': 'Yahoo-1234',
-                'email': 'Yahoo-1234@example.com',
+        with patch('login_facebook_index.FacebookUtil') as facebook_mock, \
+         patch('login_facebook_index.UserUtil') as user_mock:
+            facebook_mock.return_value.get_user_info.return_value = {
+                'user_id': 'Facebook-1234',
+                'email': 'Facebook-1234@example.com',
                 'display_name': 'my_name'
             }
-            yahoo_mock.return_value.get_access_token.return_value = {
+            facebook_mock.return_value.get_access_token.return_value = {
                 'access_token': 'access_token',
                 'id_token': 'id_token'
             }
-            yahoo_mock.return_value.verify_state_nonce.return_value = True
-            yahoo_mock.return_value.verify_access_token.return_value = True
+            facebook_mock.return_value.verify_state_nonce.return_value = True
+
             user_mock.get_external_provider_password.return_value = 'password'
             user_mock.exists_user.return_value = True
             user_mock.has_user_id.return_value = False
@@ -199,7 +198,7 @@ class TestLoginYahooIndex(TestCase):
                 }
             }
             params['body'] = json.dumps(params['body'])
-            response = LoginYahooIndex(params, {}, dynamodb=dynamodb).main()
+            response = LoginFacebookIndex(params, {}, dynamodb=dynamodb).main()
             self.assertEqual(response['statusCode'], 200)
             self.assertEqual(
                 json.loads(response['body']),
@@ -207,7 +206,7 @@ class TestLoginYahooIndex(TestCase):
                     'access_token': 'aaaaa',
                     'id_token': 'bbbbb',
                     'refresh_token': 'ccccc',
-                    'last_auth_user': 'Yahoo-1234',
+                    'last_auth_user': 'Facebook-1234',
                     'has_user_id': False,
                     'status': 'login'
                 }
@@ -216,14 +215,14 @@ class TestLoginYahooIndex(TestCase):
                 cognito=None,
                 password='password',
                 provider='xxxxx',
-                user_id='Yahoo-1234',
+                user_id='Facebook-1234',
                 user_pool_app_id='user_pool_id',
                 user_pool_id='user_pool_id'
             )
 
     def test_main_ng_with_auth_error(self):
-        with patch('login_yahoo_index.YahooUtil') as yahoo_mock:
-            yahoo_mock.return_value.get_user_info.side_effect = YahooOauthError(
+        with patch('login_facebook_index.FacebookUtil') as facebook_mock:
+            facebook_mock.return_value.get_user_info.side_effect = FacebookOauthError(
                 endpoint='http://example.com',
                 status_code=401,
                 message='auth error'
@@ -235,7 +234,7 @@ class TestLoginYahooIndex(TestCase):
                 }
             }
             params['body'] = json.dumps(params['body'])
-            response = LoginYahooIndex(params, {}).main()
+            response = LoginFacebookIndex(params, {}).main()
             self.assertEqual(response['statusCode'], 401)
             self.assertEqual(
                 json.loads(response['body']),
@@ -244,9 +243,9 @@ class TestLoginYahooIndex(TestCase):
                 }
             )
 
-    def test_main_ng_with_yahooexception(self):
-        with patch('login_yahoo_index.YahooUtil') as yahoo_mock:
-            yahoo_mock.return_value.get_user_info.side_effect = YahooOauthError(
+    def test_main_ng_with_facebookexception(self):
+        with patch('login_facebook_index.FacebookUtil') as facebook_mock:
+            facebook_mock.return_value.get_user_info.side_effect = FacebookOauthError(
                 endpoint='http://example.com',
                 status_code=500,
                 message='error'
@@ -258,7 +257,7 @@ class TestLoginYahooIndex(TestCase):
                 }
             }
             params['body'] = json.dumps(params['body'])
-            response = LoginYahooIndex(params, {}).main()
+            response = LoginFacebookIndex(params, {}).main()
             self.assertEqual(response['statusCode'], 500)
             self.assertEqual(
                 json.loads(response['body']),
@@ -268,19 +267,19 @@ class TestLoginYahooIndex(TestCase):
             )
 
     def test_main_ng_with_invalid_state_and_existing_user(self):
-        with patch('login_yahoo_index.YahooUtil') as yahoo_mock, \
-         patch('login_yahoo_index.UserUtil') as user_mock:
-            yahoo_mock.return_value.get_user_info.return_value = {
-                'user_id': 'yahoo-1234',
-                'email': 'yahoo-1234@example.com',
+        with patch('login_facebook_index.FacebookUtil') as facebook_mock, \
+         patch('login_facebook_index.UserUtil') as user_mock:
+            facebook_mock.return_value.get_user_info.return_value = {
+                'user_id': 'facebook-1234',
+                'email': 'facebook-1234@example.com',
                 'display_name': 'my_name'
             }
-            yahoo_mock.return_value.get_access_token.return_value = {
+            facebook_mock.return_value.get_access_token.return_value = {
                 'access_token': 'access_token',
                 'id_token': 'id_token'
             }
-            yahoo_mock.return_value.verify_state_nonce.return_value = False
-            yahoo_mock.return_value.verify_access_token.return_value = True
+            facebook_mock.return_value.verify_state_nonce.return_value = False
+
             user_mock.get_external_provider_password.return_value = 'password'
             user_mock.exists_user.return_value = True
             user_mock.has_user_id.return_value = True
@@ -295,7 +294,7 @@ class TestLoginYahooIndex(TestCase):
                 }
             }
             params['body'] = json.dumps(params['body'])
-            response = LoginYahooIndex(params, {}).main()
+            response = LoginFacebookIndex(params, {}).main()
             self.assertEqual(response['statusCode'], 500)
             self.assertEqual(
                 json.loads(response['body']),
@@ -305,11 +304,11 @@ class TestLoginYahooIndex(TestCase):
             )
 
     def test_main_ng_with_awsexception_and_existing_user(self):
-        with patch('login_yahoo_index.YahooUtil') as yahoo_mock, \
-         patch('login_yahoo_index.UserUtil') as user_mock:
-            yahoo_mock.return_value.get_user_info.return_value = {
-                'user_id': 'yahoo-1234',
-                'email': 'yahoo-1234@example.com',
+        with patch('login_facebook_index.FacebookUtil') as facebook_mock, \
+         patch('login_facebook_index.UserUtil') as user_mock:
+            facebook_mock.return_value.get_user_info.return_value = {
+                'user_id': 'facebook-1234',
+                'email': 'facebook-1234@example.com',
                 'display_name': 'my_name'
             }
             user_mock.exists_user.return_value = True
@@ -325,7 +324,7 @@ class TestLoginYahooIndex(TestCase):
                 }
             }
             params['body'] = json.dumps(params['body'])
-            response = LoginYahooIndex(params, {}).main()
+            response = LoginFacebookIndex(params, {}).main()
             self.assertEqual(response['statusCode'], 500)
             self.assertEqual(
                 json.loads(response['body']),
@@ -335,19 +334,19 @@ class TestLoginYahooIndex(TestCase):
             )
 
     def test_main_ng_with_awsexception_and_new_user(self):
-        with patch('login_yahoo_index.YahooUtil') as yahoo_mock, \
-         patch('login_yahoo_index.UserUtil') as user_mock:
-            yahoo_mock.return_value.get_user_info.return_value = {
-                'user_id': 'yahoo-1234',
-                'email': 'yahoo-1234@example.com',
+        with patch('login_facebook_index.FacebookUtil') as facebook_mock, \
+         patch('login_facebook_index.UserUtil') as user_mock:
+            facebook_mock.return_value.get_user_info.return_value = {
+                'user_id': 'facebook-1234',
+                'email': 'facebook-1234@example.com',
                 'display_name': 'my_name'
             }
-            yahoo_mock.return_value.get_access_token.return_value = {
+            facebook_mock.return_value.get_access_token.return_value = {
                 'access_token': 'access_token',
                 'id_token': 'id_token'
             }
-            yahoo_mock.return_value.verify_state_nonce.return_value = True
-            yahoo_mock.return_value.verify_access_token.return_value = True
+            facebook_mock.return_value.verify_state_nonce.return_value = True
+
             user_mock.get_external_provider_password.return_value = 'password'
             user_mock.exists_user.return_value = False
             user_mock.create_external_provider_user.return_value = ClientError(
@@ -365,7 +364,7 @@ class TestLoginYahooIndex(TestCase):
                 }
             }
             params['body'] = json.dumps(params['body'])
-            response = LoginYahooIndex(params, {}).main()
+            response = LoginFacebookIndex(params, {}).main()
             self.assertEqual(response['statusCode'], 500)
             self.assertEqual(
                 json.loads(response['body']),
