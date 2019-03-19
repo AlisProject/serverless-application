91757a2bff7923ac25a96df674493b618d71aa5a
y-temp4 <y.temp4@gmail.com>
2019-03-19 14:32:36 +0900

    Add get price API

---
+++ src/handlers/articles/price/show/articles_price_show.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import settings
+from db_util import DBUtil
+from lambda_base import LambdaBase
+from jsonschema import validate, ValidationError
+from decimal_encoder import DecimalEncoder
+
+
+class ArticlesPriceShow(LambdaBase):
+    def get_schema(self):
+        return {
+            'type': 'object',
+            'properties': {
+                'article_id': settings.parameters['article_id']
+            },
+            'required': ['article_id']
+        }
+
+    def validate_params(self):
+        # single
+        params = self.event.get('pathParameters')
+
+        if params is None:
+            raise ValidationError('pathParameters is required')
+
+        validate(params, self.get_schema())
+        # relation
+        DBUtil.validate_article_existence(
+            self.dynamodb,
+            params['article_id'],
+            status='public'
+        )
+
+    def exec_main_proc(self):
+        params = self.event.get('pathParameters')
+
+        article_info_table = self.dynamodb.Table(os.environ['ARTICLE_INFO_TABLE_NAME'])
+        article_info = article_info_table.get_item(Key={'article_id': params['article_id']}).get('Item')
+
+        if article_info is None:
+            return {
+               'statusCode': 404,
+               'body': json.dumps({'message': 'Record Not Found'})
+            }
+
+        response = {
+            'article_id': params['article_id'],
+            'price': params['price']
+        }
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response, cls=DecimalEncoder)
+        }
