0e3437c140b32daad4bc0fbd000fbfcaf1232cd6
y-temp4 <y.temp4@gmail.com>
2019-03-21 16:57:50 +0900

    Add MeArticlesPurchasedShow API

---
+++ src/handlers/me/articles/purchased/show/me_articles_purchased_show.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import settings
+from lambda_base import LambdaBase
+from jsonschema import validate
+from decimal_encoder import DecimalEncoder
+from db_util import DBUtil
+
+
+class MeArticlesPurchasedShow(LambdaBase):
+    def get_schema(self):
+        return {
+            'type': 'object',
+            'properties': {
+                'article_id': settings.parameters['article_id']
+            },
+            'required': ['article_id']
+        }
+
+    def validate_params(self):
+        validate(self.params, self.get_schema())
+
+        DBUtil.validate_article_existence(
+            self.dynamodb,
+            self.params['article_id'],
+            status='public',
+            is_purchased=True
+        )
+        DBUtil.validate_user_existence(
+            self.dynamodb,
+            self.event['requestContext']['authorizer']['claims']['cognito:username']
+        )
+
+    def exec_main_proc(self):
+        article_info_table = self.dynamodb.Table(os.environ['ARTICLE_INFO_TABLE_NAME'])
+        article_content_table = self.dynamodb.Table(os.environ['ARTICLE_CONTENT_TABLE_NAME'])
+
+        article_info = article_info_table.get_item(Key={'article_id': self.params['article_id']}).get('Item')
+        article_content = article_content_table.get_item(Key={'article_id': self.params['article_id']}).get('Item')
+
+        article_content['body'] = article_content['paid_body']
+        article_content.pop('paid_body', None)
+
+        article_info.update(article_content)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(article_info, cls=DecimalEncoder)
+        }
---
+++ tests/handlers/me/articles/purchased/show/test_me_articles_purchased_show.py
@@ -0,0 +1,171 @@
+import os
+import json
+from unittest import TestCase
+from me_articles_purchased_show import MeArticlesPurchasedShow
+from unittest.mock import patch, MagicMock
+from tests_util import TestsUtil
+
+
+class TestMeArticlesPurchasedShow(TestCase):
+    dynamodb = TestsUtil.get_dynamodb_client()
+
+    @classmethod
+    def setUpClass(cls):
+        TestsUtil.set_all_tables_name_to_env()
+        TestsUtil.delete_all_tables(cls.dynamodb)
+
+        users_table_items = [
+            {
+                'user_id': 'test01'
+            }
+        ]
+        TestsUtil.create_table(cls.dynamodb, os.environ['USERS_TABLE_NAME'], users_table_items)
+
+        article_info_items = [
+            {
+                'article_id': 'publicId0001',
+                'user_id': 'test01',
+                'status': 'public',
+                'sort_key': 1520150272000000,
+                'overview': 'sample_overview1',
+                'eye_catch_url': 'http://example.com/eye_catch_url',
+                'price': 100
+            },
+            {
+                'article_id': 'publicId0002',
+                'user_id': 'test02',
+                'status': 'public',
+                'sort_key': 1520150272000001,
+                'overview': 'sample_overview2',
+                'eye_catch_url': 'http://example.com/eye_catch_url',
+            }
+        ]
+        TestsUtil.create_table(cls.dynamodb, os.environ['ARTICLE_INFO_TABLE_NAME'], article_info_items)
+
+        article_content_items = [
+            {
+                'article_id': 'publicId0001',
+                'title': 'sample_title1',
+                'body': 'sample_body1',
+                'paid_body': 'sample_paid_body1'
+            },
+            {
+                'article_id': 'publicId0002',
+                'title': 'sample_title2',
+                'body': 'sample_body2',
+            }
+        ]
+        TestsUtil.create_table(cls.dynamodb, os.environ['ARTICLE_CONTENT_TABLE_NAME'], article_content_items)
+
+    @classmethod
+    def tearDownClass(cls):
+        TestsUtil.delete_all_tables(cls.dynamodb)
+
+    def assert_bad_request(self, params):
+        function = MeArticlesPurchasedShow(params, {}, self.dynamodb)
+        response = function.main()
+
+        self.assertEqual(response['statusCode'], 400)
+
+    def test_main_ok(self):
+        params = {
+            'pathParameters': {
+                'article_id': 'publicId0001'
+            },
+            'requestContext': {
+                'authorizer': {
+                    'claims': {
+                        'cognito:username': 'test01'
+                    }
+                }
+            }
+        }
+
+        response = MeArticlesPurchasedShow(params, {}, self.dynamodb).main()
+
+        expected_item = {
+            'article_id': 'publicId0001',
+            'user_id': 'test01',
+            'title': 'sample_title1',
+            'body': 'sample_paid_body1',
+            'status': 'public',
+            'overview': 'sample_overview1',
+            'sort_key': 1520150272000000,
+            'eye_catch_url': 'http://example.com/eye_catch_url',
+            'price': 100
+        }
+
+        self.assertEqual(response['statusCode'], 200)
+        self.assertEqual(json.loads(response['body']), expected_item)
+
+    def test_call_validate_article_existence(self):
+        params = {
+            'pathParameters': {
+                'article_id': 'publicId0001'
+            },
+            'requestContext': {
+                'authorizer': {
+                    'claims': {
+                        'cognito:username': 'test01'
+                    }
+                }
+            }
+        }
+
+        mock_lib = MagicMock()
+        with patch('me_articles_purchased_show.DBUtil', mock_lib):
+            MeArticlesPurchasedShow(params, {}, self.dynamodb).main()
+            args, kwargs = mock_lib.validate_article_existence.call_args
+
+            self.assertTrue(mock_lib.validate_article_existence.called)
+            self.assertTrue(args[0])
+            self.assertTrue(args[1])
+            self.assertEqual(kwargs['status'], 'public')
+            self.assertTrue(kwargs['is_purchased'])
+
+    def test_validation_with_no_params(self):
+        params = {
+            'pathParameters': {}
+        }
+
+        self.assert_bad_request(params)
+
+    def test_validation_not_article_exists(self):
+        params = {
+            'pathParameters': {
+                'article_id': 'publicId0003'
+            }
+        }
+
+        response = MeArticlesPurchasedShow(params, {}, self.dynamodb).main()
+
+        self.assertEqual(response['statusCode'], 404)
+
+    def test_validation_not_paid_article(self):
+        params = {
+            'pathParameters': {
+                'article_id': 'publicId0002'
+            }
+        }
+
+        response = MeArticlesPurchasedShow(params, {}, self.dynamodb).main()
+
+        self.assertEqual(response['statusCode'], 404)
+
+    def test_validation_article_id_max(self):
+        params = {
+            'queryStringParameters': {
+                'article_id': 'A' * 13
+            }
+        }
+
+        self.assert_bad_request(params)
+
+    def test_validation_article_id_min(self):
+        params = {
+            'queryStringParameters': {
+                'article_id': 'A' * 11
+            }
+        }
+
+        self.assert_bad_request(params)
