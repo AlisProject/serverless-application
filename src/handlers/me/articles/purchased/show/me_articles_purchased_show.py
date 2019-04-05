6a4cd11cddbd62cae8bb59e2a0b996ac2d08e439
y-temp4 <y.temp4@gmail.com>
2019-04-05 18:05:42 +0900

    Fix comment

--- src/handlers/me/articles/purchased/show/me_articles_purchased_show.py
+++ src/handlers/me/articles/purchased/show/me_articles_purchased_show.py
@@ -46,7 +46,7 @@ class MeArticlesPurchasedShow(LambdaBase):
         article_info = article_info_table.get_item(Key={'article_id': self.params['article_id']}).get('Item')
         article_content = article_content_table.get_item(Key={'article_id': self.params['article_id']}).get('Item')
 
-        # 記事が有料から無料になるケースを考慮し、有料記事の場合のみ body の書き換えを行う
+        # 記事が有料から無料になるケースを考慮し、無料記事の場合は本文（body）をそのまま返却する
         if 'price' in article_info:
             article_content['body'] = article_content['paid_body']
             article_content.pop('paid_body', None)
