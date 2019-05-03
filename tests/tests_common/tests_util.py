460192d74bcacf5bda3c2c5bda0856e495bb41f8
matsumatsu20 <matsumatsu20@gmail.com>
2019-05-03 18:39:22 +0900

    Create endpoint to get articles sorted by tip quantity

--- tests/tests_common/tests_util.py
+++ tests/tests_common/tests_util.py
@@ -9,7 +9,7 @@ class TestsUtil:
     @staticmethod
     def create_table(dynamodb, table_name, table_items):
         # create table
-        f = open('./database.yaml', 'r+')
+        f = open('/Users/matsumatsu20/Develop/alis/serverless-application/database.yaml', 'r+')
         template = yaml.load(f)
         f.close()
 
@@ -30,7 +30,7 @@ class TestsUtil:
     def get_dynamodb_client():
         if os.environ.get('IS_DYNAMODB_ENDPOINT_OF_AWS') is not None:
             return boto3.resource('dynamodb')
-        return boto3.resource('dynamodb', endpoint_url='http://localhost:8000/')
+        return boto3.resource('dynamodb', endpoint_url='http://localhost:8000/', region_name="ap-northeast-1")
 
     @classmethod
     def create_all_s3_buckets(cls, s3):
