bb7dab4a718887e2ed2104bf30c408942b4a5ae6
matsumatsu20 <matsumatsu20@gmail.com>
2019-05-28 23:38:39 +0900

    Add burn logic in me_wallet_tip

--- tests/handlers/me/wallet/tip/test_me_wallet_tip.py
+++ tests/handlers/me/wallet/tip/test_me_wallet_tip.py
@@ -47,6 +47,8 @@ class TestMeWalletTip(TestCase):
 
     @patch('me_wallet_tip.MeWalletTip._MeWalletTip__send_tip',
            MagicMock(return_value='0x0000000000000000000000000000000000000000'))
+    @patch('private_chain_util.PrivateChainUtil.is_transaction_completed', MagicMock(return_value=True))
+    @patch('me_wallet_tip.MeWalletTip._MeWalletTip__burn_transaction', MagicMock(return_value='burn_transaction_hash'))
     @patch('time_util.TimeUtil.generate_sort_key', MagicMock(return_value=1520150552000003))
     @patch('time.time', MagicMock(return_value=1520150552.000003))
     def test_main_ok_min_value(self):
@@ -92,6 +94,7 @@ class TestMeWalletTip(TestCase):
                 'article_id': target_article_id,
                 'article_title': self.article_info_table_items[0]['title'],
                 'transaction': '0x0000000000000000000000000000000000000000',
+                'burn_transaction': 'burn_transaction_hash',
                 'uncompleted': Decimal(1),
                 'sort_key': Decimal(1520150552000003),
                 'target_date': '2018-03-04',
@@ -105,13 +108,77 @@ class TestMeWalletTip(TestCase):
     @patch('time_util.TimeUtil.generate_sort_key', MagicMock(return_value=1520150552000003))
     @patch('time.time', MagicMock(return_value=1520150552.000003))
     def test_main_ok_max_value(self):
-        with patch('me_wallet_tip.UserUtil') as user_util_mock:
+        with patch('me_wallet_tip.UserUtil') as user_util_mock, \
+             patch('private_chain_util.PrivateChainUtil.is_transaction_completed') as mock_is_transaction_completed, \
+             patch('me_wallet_tip.MeWalletTip._MeWalletTip__burn_transaction') as mock_burn_transaction:
             user_util_mock.get_cognito_user_info.return_value = {
                 'UserAttributes': [{
                     'Name': 'custom:private_eth_address',
                     'Value': '0x1111111111111111111111111111111111111111'
                 }]
             }
+            mock_is_transaction_completed.return_value = True
+            mock_burn_transaction.return_value = 'burn_transaction_hash'
+
+            target_article_id = self.article_info_table_items[0]['article_id']
+            target_tip_value = str(settings.parameters['tip_value']['maximum'])
+
+            event = {
+                'body': {
+                    'article_id': target_article_id,
+                    'tip_value': target_tip_value
+                },
+                'requestContext': {
+                    'authorizer': {
+                        'claims': {
+                            'cognito:username': 'act_user_01',
+                            'custom:private_eth_address': '0x5d7743a4a6f21593ff6d3d81595f270123456789',
+                            'phone_number_verified': 'true',
+                            'email_verified': 'true'
+                        }
+                    }
+                }
+            }
+            event['body'] = json.dumps(event['body'])
+
+            response = MeWalletTip(event, {}, self.dynamodb, cognito=None).main()
+            self.assertEqual(mock_is_transaction_completed.call_count, 1)
+            self.assertEqual(mock_burn_transaction.call_count, 1)
+
+            self.assertEqual(response['statusCode'], 200)
+            tip_table = self.dynamodb.Table(os.environ['TIP_TABLE_NAME'])
+            tips = tip_table.scan()['Items']
+            self.assertEqual(len(tips), 1)
+
+            expected_tip = {
+                'user_id': event['requestContext']['authorizer']['claims']['cognito:username'],
+                'to_user_id': self.article_info_table_items[0]['user_id'],
+                'tip_value': Decimal(target_tip_value),
+                'article_id': target_article_id,
+                'article_title': self.article_info_table_items[0]['title'],
+                'transaction': '0x0000000000000000000000000000000000000000',
+                'burn_transaction': 'burn_transaction_hash',
+                'uncompleted': Decimal(1),
+                'sort_key': 1520150552000003,
+                'target_date': '2018-03-04',
+                'created_at': Decimal(int(1520150552.000003))
+            }
+
+            self.assertEqual(expected_tip, tips[0])
+
+    @patch('me_wallet_tip.MeWalletTip._MeWalletTip__send_tip',
+           MagicMock(return_value='0x0000000000000000000000000000000000000000'))
+    @patch('private_chain_util.PrivateChainUtil.is_transaction_completed', MagicMock(return_value=False))
+    def test_main_ok_with_wrong_transaction_status(self):
+        with patch('me_wallet_tip.UserUtil') as user_util_mock, \
+             patch('me_wallet_tip.MeWalletTip._MeWalletTip__burn_transaction') as mock_burn_transaction:
+            user_util_mock.get_cognito_user_info.return_value = {
+                'UserAttributes': [{
+                    'Name': 'custom:private_eth_address',
+                    'Value': '0x1111111111111111111111111111111111111111'
+                }]
+            }
+            mock_burn_transaction.return_value = 'burn_transaction_hash'
 
             target_article_id = self.article_info_table_items[0]['article_id']
             target_tip_value = str(settings.parameters['tip_value']['maximum'])
@@ -136,6 +203,49 @@ class TestMeWalletTip(TestCase):
 
             response = MeWalletTip(event, {}, self.dynamodb, cognito=None).main()
             self.assertEqual(response['statusCode'], 200)
+
+            self.assertEqual(mock_burn_transaction.call_count, 0)
+
+    @patch('me_wallet_tip.MeWalletTip._MeWalletTip__send_tip',
+           MagicMock(return_value='0x0000000000000000000000000000000000000000'))
+    @patch('private_chain_util.PrivateChainUtil.is_transaction_completed', MagicMock(side_effect=Exception()))
+    @patch('time_util.TimeUtil.generate_sort_key', MagicMock(return_value=1520150552000003))
+    @patch('time.time', MagicMock(return_value=1520150552.000003))
+    def test_main_ok_with_exception_in_is_transaction_completed(self):
+        with patch('me_wallet_tip.UserUtil') as user_util_mock, \
+             patch('me_wallet_tip.MeWalletTip._MeWalletTip__burn_transaction') as mock_burn_transaction:
+            user_util_mock.get_cognito_user_info.return_value = {
+                'UserAttributes': [{
+                    'Name': 'custom:private_eth_address',
+                    'Value': '0x1111111111111111111111111111111111111111'
+                }]
+            }
+            mock_burn_transaction.return_value = 'burn_transaction_hash'
+
+            target_article_id = self.article_info_table_items[0]['article_id']
+            target_tip_value = str(settings.parameters['tip_value']['maximum'])
+
+            event = {
+                'body': {
+                    'article_id': target_article_id,
+                    'tip_value': target_tip_value
+                },
+                'requestContext': {
+                    'authorizer': {
+                        'claims': {
+                            'cognito:username': 'act_user_01',
+                            'custom:private_eth_address': '0x5d7743a4a6f21593ff6d3d81595f270123456789',
+                            'phone_number_verified': 'true',
+                            'email_verified': 'true'
+                        }
+                    }
+                }
+            }
+            event['body'] = json.dumps(event['body'])
+
+            response = MeWalletTip(event, {}, self.dynamodb, cognito=None).main()
+            self.assertEqual(response['statusCode'], 200)
+
             tip_table = self.dynamodb.Table(os.environ['TIP_TABLE_NAME'])
             tips = tip_table.scan()['Items']
             self.assertEqual(len(tips), 1)
@@ -147,6 +257,7 @@ class TestMeWalletTip(TestCase):
                 'article_id': target_article_id,
                 'article_title': self.article_info_table_items[0]['title'],
                 'transaction': '0x0000000000000000000000000000000000000000',
+                'burn_transaction': None,  # 途中で処理失敗しているためNoneが入る
                 'uncompleted': Decimal(1),
                 'sort_key': 1520150552000003,
                 'target_date': '2018-03-04',
