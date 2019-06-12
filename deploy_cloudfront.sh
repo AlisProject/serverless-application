3e5d246475f8061f4f684ebc49f975e8138c43d8
sot528 <sot528@gmail.com>
2019-06-12 14:33:02 +0900

    add Cloudfront resources

---
+++ deploy_cloudfront.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#aws cloudformation package \
+#  --template-file ${target}template.yaml \
+#  --s3-bucket $DEPLOY_BUCKET_NAME \
+#  --output-template-file ${target}packaged-template.yaml
+
+#  --s3-bucket $DEPLOY_BUCKET_NAME \
+#  --template-file ${target}packaged-template.yaml \
+
+aws cloudformation deploy \
+  --template-file cloudfront-template.yaml \
+  --stack-name ${ALIS_APP_ID}-cloudfront \
+  --parameter-overrides \
+    AlisAppId=${ALIS_APP_ID} \
+    ApiApiGatewayId=${SERVERLESS_REST_API_ID} \
+    FrontendApiGatewayId=${FrontendApiGatewayId} \
+    Oauth2ApiGatewayId=${Oauth2ApiGatewayId} \
+    Oauth2apiApiGatewayId=${Oauth2apiApiGatewayId} \
+    LaboApiGatewayId=${LaboApiGatewayId} \
+    AcmCertificateArn=${AcmCertificateArn} \
+  --capabilities CAPABILITY_IAM \
+  --no-fail-on-empty-changeset
