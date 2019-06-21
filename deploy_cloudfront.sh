987eb1343cf9bcefbc01c9c2ee39b70b598ee52d
sot528 <sot528@gmail.com>
2019-06-21 22:06:05 +0900

    fix deploy script

--- deploy_cloudfront.sh
+++ deploy_cloudfront.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-#aws cloudformation package \
-#  --template-file ${target}template.yaml \
-#  --s3-bucket $DEPLOY_BUCKET_NAME \
-#  --output-template-file ${target}packaged-template.yaml
-
-#  --s3-bucket $DEPLOY_BUCKET_NAME \
-#  --template-file ${target}packaged-template.yaml \
-
 aws cloudformation deploy \
   --template-file cloudfront-template.yaml \
   --stack-name ${ALIS_APP_ID}-cloudfront \
@@ -19,6 +11,5 @@ aws cloudformation deploy \
     Oauth2apiApiGatewayId=${Oauth2apiApiGatewayId} \
     LaboApiGatewayId=${LaboApiGatewayId} \
     AcmCertificateArn=${AcmCertificateArn} \
-    Route53HostZoneId=${Route53HostZoneId} \
   --capabilities CAPABILITY_IAM \
   --no-fail-on-empty-changeset
