ea77942546c9d76bcd4051a821b64873b4ce52a1
sot528 <sot528@gmail.com>
2019-07-17 02:26:38 +0900

    reject exist user

--- src/handlers/labo/n/majority_judgement/index/majority_judgement_index.py
+++ src/handlers/labo/n/majority_judgement/index/majority_judgement_index.py
@@ -1,60 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
-import time
+import json
 from lambda_base import LambdaBase
-from jsonschema import validate
-
-options_count = 5  # 選択肢の数
-valuation_level = 7  # n段階評価の、n
 
 
 class LaboNMajorityJudgementIndex(LambdaBase):
     def get_schema(self):
-        opt = {
-            "type": "number",
-            "minimum": 1,
-            "maximum": valuation_level
-        }
-
-        required = []
-        properties = {}
-        for i in range(options_count):
-            key = 'opt_' + str(i + 1)
-            properties[key] = opt
-            required.append(key)
-
-        for i in range(options_count):
-            properties['opt_' + str(i + 1)] = opt
-
-        return {
-            "type": "object",
-            "properties": properties,
-            "required": required
-        }
+        pass
 
     def validate_params(self):
-        validate(self.params, self.get_schema())
+        pass
 
     def exec_main_proc(self):
         table = self.dynamodb.Table(os.environ['MAJORITY_JUDGEMENT_TABLE_NAME'])
 
         user_id = self.event['requestContext']['authorizer']['claims']['cognito:username']
-        if not LaboNMajorityJudgementIndex.__is_exists(table, user_id):
-            item = {
-                'user_id': user_id,
-                'created_at': int(time.time())
-            }
-
-            for key, value in self.params.items():
-                item[key] = value
-
-            table.put_item(
-                Item=item,
-                ConditionExpression='attribute_not_exists(user_id)'
-            )
+        exists = LaboNMajorityJudgementIndex.__is_exists(table, user_id)
 
         return {
-            'statusCode': 200
+            'statusCode': 200,
+            'body': json.dumps({'exists': exists})
         }
 
     @staticmethod
