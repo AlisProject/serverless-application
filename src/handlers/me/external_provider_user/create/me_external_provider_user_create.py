1c9c1af8baece3bd6a6005b7a7949d1d18da6d81
Go Hayakawa <go.hayakawa@gmail.com>
2019-09-30 15:45:09 +0900

    Fix error handling in external provider user creation

--- src/handlers/me/external_provider_user/create/me_external_provider_user_create.py
+++ src/handlers/me/external_provider_user/create/me_external_provider_user_create.py
@@ -2,10 +2,8 @@
 import json
 import os
 import settings
-import logging
 from lambda_base import LambdaBase
 from jsonschema import validate, ValidationError
-from botocore.exceptions import ClientError
 from user_util import UserUtil
 from crypto_util import CryptoUtil
 from record_not_found_error import RecordNotFoundError
@@ -58,71 +56,62 @@ class MeExternalProviderUserCreate(LambdaBase):
         elif not already_user_exists:
             # EXTERNAL_PROVIDERのidで作成したcognitoのユーザーを除去
             if UserUtil.delete_external_provider_id_cognito_user(self.cognito, external_provider_user_id):
-
                 # user_idでのCognitoユーザーの作成し直し
-                try:
-                    email = external_provider_user['email']
-                    hash_data = external_provider_user['password']
-                    byte_hash_data = hash_data.encode()
-                    decoded_iv = external_provider_user['iv']
-                    iv = decoded_iv.encode()
-                    backed_password = CryptoUtil.decrypt_password(byte_hash_data, iv)
+                email = external_provider_user['email']
+                hash_data = external_provider_user['password']
+                byte_hash_data = hash_data.encode()
+                decoded_iv = external_provider_user['iv']
+                iv = decoded_iv.encode()
+                backed_password = CryptoUtil.decrypt_password(byte_hash_data, iv)
 
-                    backed_temp_password = os.environ['EXTERNAL_PROVIDER_LOGIN_COMMON_TEMP_PASSWORD']
-                    provider = os.environ['EXTERNAL_PROVIDER_LOGIN_MARK']
+                backed_temp_password = os.environ['EXTERNAL_PROVIDER_LOGIN_COMMON_TEMP_PASSWORD']
+                provider = os.environ['EXTERNAL_PROVIDER_LOGIN_MARK']
 
-                    response = UserUtil.create_external_provider_user(
-                        cognito=self.cognito,
-                        user_id=body['user_id'],
-                        user_pool_id=os.environ['COGNITO_USER_POOL_ID'],
-                        user_pool_app_id=os.environ['COGNITO_USER_POOL_APP_ID'],
-                        email=email,
-                        backed_temp_password=backed_temp_password,
-                        backed_password=backed_password,
-                        provider=provider
-                    )
+                response = UserUtil.create_external_provider_user(
+                    cognito=self.cognito,
+                    user_id=body['user_id'],
+                    user_pool_id=os.environ['COGNITO_USER_POOL_ID'],
+                    user_pool_app_id=os.environ['COGNITO_USER_POOL_APP_ID'],
+                    email=email,
+                    backed_temp_password=backed_temp_password,
+                    backed_password=backed_password,
+                    provider=provider
+                )
 
-                    UserUtil.force_non_verified_phone(
-                        cognito=self.cognito,
-                        user_id=body['user_id']
-                    )
+                UserUtil.force_non_verified_phone(
+                    cognito=self.cognito,
+                    user_id=body['user_id']
+                )
 
-                    UserUtil.wallet_initialization(self.cognito, os.environ['COGNITO_USER_POOL_ID'], body['user_id'])
+                UserUtil.wallet_initialization(self.cognito, os.environ['COGNITO_USER_POOL_ID'], body['user_id'])
 
-                    # ExternalProviderUsersテーブルにuser_idを追加
-                    UserUtil.add_user_id_to_external_provider_user(
-                        body['user_id'],
-                        external_provider_users_table,
-                        external_provider_user_id
-                    )
+                # ExternalProviderUsersテーブルにuser_idを追加
+                UserUtil.add_user_id_to_external_provider_user(
+                    body['user_id'],
+                    external_provider_users_table,
+                    external_provider_user_id
+                )
 
-                    # Usersテーブルにユーザーを作成
-                    UserUtil.add_user_profile(
-                        dynamodb=self.dynamodb,
-                        user_id=body['user_id'],
-                        user_display_name=body['user_id']
-                    )
+                # Usersテーブルにユーザーを作成
+                UserUtil.add_user_profile(
+                    dynamodb=self.dynamodb,
+                    user_id=body['user_id'],
+                    user_display_name=body['user_id']
+                )
 
-                    has_user_id = UserUtil.has_user_id(self.dynamodb, external_provider_user_id)
+                has_user_id = UserUtil.has_user_id(self.dynamodb, external_provider_user_id)
 
-                    return {
-                        'statusCode': 200,
-                        'body': json.dumps({
-                            'access_token': response['AuthenticationResult']['AccessToken'],
-                            'last_auth_user': body['user_id'],
-                            'id_token': response['AuthenticationResult']['IdToken'],
-                            'refresh_token': response['AuthenticationResult']['RefreshToken'],
-                            'status': 'login',
-                            'has_user_id': has_user_id
-                        })
-                    }
-
-                except ClientError as e:
-                    logging.fatal(e)
-                    return {
-                        'statusCode': 500,
-                        'body': json.dumps({'message': 'Internal server error'})
-                    }
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps({
+                        'access_token': response['AuthenticationResult']['AccessToken'],
+                        'last_auth_user': body['user_id'],
+                        'id_token': response['AuthenticationResult']['IdToken'],
+                        'refresh_token': response['AuthenticationResult']['RefreshToken'],
+                        'status': 'login',
+                        'has_user_id': has_user_id
+                    })
+                }
 
             return {
                 'statusCode': 500,
